ade34874efc9db610bcc072d1d7f95afdc7cd0e6
sowmiyaa25 <kit.27.23bae053@gmail.com>
2026-01-15 20:46:42 +0530

    Delete contact.sql

--- contact.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE contact_messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    message TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
